6102431d8cc33e5f1fc5f12219c75458bcd84032
Cameron Jones <cameronjonesaustralia@gmail.com>
2024-10-16 11:05:21 +1100

    Added templating

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
 
   // This will stop the default behaviour of foo.html being turned into foo/index.html
   eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+  eleventyConfig.setTemplateFormats(["html", "njk", "txt", "js", "css", "xml", "json"]);
 
   return {
     dir: {
